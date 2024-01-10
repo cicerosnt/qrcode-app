28a26358352b7bbe1c4db04ddd35760132f0207f
Cícero Santos <cicero.dsantos@outlook.com.br>
2024-01-09 23:08:12 -0300

    update code and fixed bugs

--- main.js
+++ main.js
@@ -1,17 +1,36 @@
 function GerarQRCode() {
-  var inputUsuario = document.querySelector('textarea').value;
+  var data = document.querySelector('textarea').value;
   var output = document.querySelector('#output');
   
-  console.log(output);
-  
-  if (inputUsuario.length <= 0) {
-    alert("Por favor, informe para gerar o QGCode!");
+  if (data.length <= 0) {
+    alert("Por favor, informe uma valor para gerar o QRCode!");
     output.style.display = 'none';
     return;
   }
-  var GoogleChartAPI = 'https://chart.googleapis.com/chart?cht=qr&chs=500x500&chld=H&chl=';
-  var conteudoQRCode = GoogleChartAPI + encodeURIComponent(inputUsuario);
-  document.querySelector('#QRCodeImage').src = conteudoQRCode;
+  if (data.length >= 256) {
+    alert("A quantidade de caracteres excede o limite para o QRCode! ");
+    output.style.display = 'none';
+    return;
+  }
   
+  
+  var requestApi = 'https://chart.googleapis.com/chart?cht=qr&chs=500x500&chld=H&chl=';
+  var createdQrcode = requestApi + encodeURIComponent(data);
+  var plotImage = document.querySelector('#QRCodeImage');
+  
+  plotImage.src = createdQrcode;
+    
   output.style.display = 'block';
+  
+  var elementDown = document.getElementById("downImage");
+  
+  if(plotImage != null){
+    elementDown.href = createdQrcode+".jpg";
+    elementDown.style.display = "none"
+    // elementDown.addEventListener("click", function (e) {
+    //   elementDown.downloaded = "qrcode-app";
+    // })
+  }else{
+    elementDown.style.display = "none"
+  }
 }
